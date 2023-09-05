50981dd933cf0e349ea25b419d82749526b43059
jeffry <jeffry@jeffry.jeffry>
2023-09-05 00:03:13 -0400

    Initial commit

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,11 +1,26 @@
-import React, { useRef } from 'react';
-import emailjs from 'emailjs-com'; // Correctly import emailjs-com
+import React, { useRef, useState } from 'react';
+import emailjs from 'emailjs-com';
 import { BsInstagram, BsFacebook, BsGithub } from 'react-icons/bs';
 import './contact.css';
-import {BiLogoWhatsapp} from 'react-icons/bi'
+import { BiLogoWhatsapp } from 'react-icons/bi';
 
 const Contact = () => {
-  const form = useRef(); // Correct the ref name from 'from' to 'form'
+  const form = useRef();
+  const [formData, setFormData] = useState({
+    name: '',
+    lastname: '',
+    email: '',
+    number: '',
+    message: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -15,11 +30,22 @@ const Contact = () => {
       .then((response) => {
         console.log('Email sent successfully:', response);
         // Handle success, e.g., show a success message to the user.
+        resetForm();
       })
       .catch((error) => {
         console.error('Email send error:', error);
         // Handle error, e.g., show an error message to the user.
       });
+  };
+
+  const resetForm = () => {
+    setFormData({
+      name: '',
+      lastname: '',
+      email: '',
+      number: '',
+      message: '',
+    });
   };
 
   return (
@@ -37,28 +63,71 @@ const Contact = () => {
                 <div className='responsive-container-block'>
                   <div className='responsive-cell-block wk-ipadp-6 wk-tab-12 wk-mobile-12 wk-desk-6' id='i10mt'>
                     <p className='text-blk input-title'>FIRST NAME</p>
-                    <input className='input' id='ijowk' name='name' placeholder='Please enter first name...' />
+                    <input
+                      className='input'
+                      id='ijowk'
+                      name='name'
+                      placeholder='Please enter first name...'
+                      value={formData.name}
+                      onChange={handleChange}
+                    />
                   </div>
                   <div className='responsive-cell-block wk-desk-6 wk-ipadp-6 wk-tab-12 wk-mobile-12'>
                     <p className='text-blk input-title'>LAST NAME</p>
-                    <input className='input' id='indfi' name='lastname' placeholder='Please enter last name...' />
+                    <input
+                      className='input'
+                      id='indfi'
+                      name='lastname'
+                      placeholder='Please enter last name...'
+                      value={formData.lastname}
+                      onChange={handleChange}
+                    />
                   </div>
                   <div className='responsive-cell-block wk-desk-6 wk-ipadp-6 wk-tab-12 wk-mobile-12'>
                     <p className='text-blk input-title'>EMAIL</p>
-                    <input className='input' id='ipmgh' name='email' placeholder='Please enter email...' />
+                    <input
+                      className='input'
+                      id='ipmgh'
+                      name='email'
+                      placeholder='Please enter email...'
+                      value={formData.email}
+                      onChange={handleChange}
+                    />
                   </div>
                   <div className='responsive-cell-block wk-desk-6 wk-ipadp-6 wk-tab-12 wk-mobile-12'>
                     <p className='text-blk input-title'>PHONE NUMBER</p>
-                    <input className='input' id='imgis' name='number' placeholder='Please enter phone number...' />
+                    <input
+                      className='input'
+                      id='imgis'
+                      name='number'
+                      placeholder='Please enter phone number...'
+                      value={formData.number}
+                      onChange={handleChange}
+                    />
                   </div>
                   <div className='responsive-cell-block wk-tab-12 wk-mobile-12 wk-desk-12 wk-ipadp-12' id='i634i'>
                     <p className='text-blk input-title'>WHAT DO YOU HAVE IN MIND</p>
-                    <textarea className='textinput' id='i5vyy' name='message' placeholder='Please enter query...' defaultValue={''} />
+                    <textarea
+                      className='textinput'
+                      id='i5vyy'
+                      name='message'
+                      placeholder='Please enter query...'
+                      value={formData.message}
+                      onChange={handleChange}
+                    />
                   </div>
                 </div>
-                <button className='submit-btn'>Submit</button>
+                <div className='button-group'>
+                  <button className='submit-btn' type='submit'>
+                    Submit
+                  </button>
+                  <button className='clr-btn' type='button' onClick={resetForm}>
+                    Clear
+                  </button>
+                </div>
               </div>
-              <div className='social-media-links'>
+            </form>
+            <div className='social-media-links'>
                 <a href='https://www.instagram.com/salman____faris.___/' id='ix94i-2'>
                   <BsInstagram className='link-img' alt='Instagram' />
                 </a>
@@ -70,11 +139,11 @@ const Contact = () => {
                 </a>
                 <a href='https://wa.link/5yyril'>
                   <BiLogoWhatsapp className='link-img' alt='Github' />
-                </a>
-              </div>
-            </form>
+                </a></div>
           </div>
+          
         </div>
+        
       </div>
     </section>
   );
